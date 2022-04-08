b4dca6f732f54c81cfe345cea1053a03ad17e0d9
sblackley <sblackley1@gmail.com>
2022-04-07 20:14:04 -0400

    Working test build

--- app.js
+++ app.js
@@ -11,47 +11,119 @@ var targetBays = 0;
 // Use bay values to calculate current service percentage
 function calculateCurrentPercentage() {
     let total = parseInt(greenBaysValue) + parseInt(yellowBaysValue) + parseInt(redBaysValue);
+
     let green = parseInt(greenBaysValue) + parseInt(yellowBaysValue);
    
     if (greenBaysValue > 1 && yellowBaysValue > 1 && redBaysValue > 1) {
 
-
         currentPercentageOutput = document.getElementById('currentPercentageOutput');
+
         currentPercentage = ((green/total) * 100);
-        currentPercentageOutput.textContent = currentPercentage;
-        
+        currentPercentageOutput.textContent = currentPercentage.toFixed(1) + "%";
     }
+
     else {
-        document.getElementById('currentPercentageOutput').textContent = "";
+        document.getElementById('currentPercentageOutput').textContent = "---";
     }
 }
 
 function calculateBaysPerEmployee() {
+
     let numEmployees = parseInt(document.getElementById('numEmployees').value);
     
     if (numEmployees >= 1) {
-        document.getElementById('baysPerEmployeeOutput').textContent = targetBays/Math.floor(numEmployees);
+    
+        document.getElementById('baysPerEmployeeOutput').textContent = (targetBays/Math.floor(numEmployees)).toFixed(1);
+
+        calculateTargetBaysTomorrow();
+    }
+    else {
+    
+        document.getElementById('baysPerEmployeeOutput').textContent = "---";
+    
     }
 }
 
 function calculateTargetBays() {
-
+    // Current percentage has already been calculated; bay numbers have been parsed and set
     if (currentPercentage > 1) {
+    
         let total = parseInt(greenBaysValue) + parseInt(yellowBaysValue) + parseInt(redBaysValue);
-        let completedBays = greenBaysValue;
+        
+        let completedBays = parseInt(greenBaysValue) + parseInt(yellowBaysValue);
 
+        // Store target percentage user input
         targetPercentage = parseInt(document.getElementById("targetPercentage").value);
+
         if (targetPercentage > currentPercentage) {
+            
             targetBays = ((targetPercentage/100) * total) - completedBays;
-            console.log(targetBays);
-            parseInt(document.getElementById('targetPercentageOutput').textContent) = targetBays;
+            
+            // Update page with target bays
+            document.getElementById('targetPercentageOutput').textContent = targetBays.toFixed(1);
+            
         }
         else {
-            document.getElementById('targetPercentageOutput').textContent = "";
+
+            document.getElementById('targetPercentageOutput').textContent = "---";
+        
         }
     }
 
 }
+
+function calculateTargetBaysTomorrow() {
+    // Target bays are calculated
+    let numEmployees = parseInt(document.getElementById('numEmployees').value);
+
+    if (targetBays > 1) {
+        
+        let total = parseInt(greenBaysValue) + parseInt(yellowBaysValue) + parseInt(redBaysValue);
+        let targetBaysNextDay = ((targetPercentage/100) * total) - parseInt(greenBaysValue)
+        document.getElementById('targetBaysNextDayOutput').textContent = targetBaysNextDay;
+        
+        if (numEmployees >= 1) {
+
+            // Calculate number of bays per employee needed to reach by tomorrow
+            document.getElementById('baysPerEmployeeNextDayOutput').textContent = (targetBaysNextDay/Math.floor(numEmployees)).toFixed(1);
+        
+        }
+        else {
+        
+            document.getElementById('baysPerEmployeeNextDayOutput').textContent = "---";
+        
+        }
+        
+    }
+}
+
+
+
+// Update variable with value of greenBays text field
+function updateGreen() {
+    greenBaysValue = document.getElementById('greenBays').value;
+    
+    calculateCurrentPercentage();
+}
+
+// Update variable with value of yellowBays text field
+function updateYellow() {
+    yellowBaysValue = document.getElementById('yellowBays').value;
+    
+    calculateCurrentPercentage();
+}
+
+// Update variable with value of redBays text field
+function updateRed() {
+    redBaysValue = document.getElementById('redBays').value;
+    
+    calculateCurrentPercentage();
+}
+
+
+
+
+/* 
 
 // Update variable with value of targetPercentage text field
 function updateTargetPercentage() {
@@ -62,67 +134,4 @@ function updateTargetPercentage() {
         }
     }
 }
-
-// Update variable with value of greenBays text field
-function updateGreen() {
-    greenBaysValue = document.getElementById('greenBays').value;
-    console.log(greenBaysValue);
-    calculateCurrentPercentage();
-}
-
-// Update variable with value of yellowBays text field
-function updateYellow() {
-    yellowBaysValue = document.getElementById('yellowBays').value;
-    console.log(yellowBaysValue);
-    calculateCurrentPercentage();
-}
-
-// Update variable with value of redBays text field
-function updateRed() {
-    redBaysValue = document.getElementById('redBays').value;
-    console.log(redBaysValue);
-    calculateCurrentPercentage();
-}
-
-// Perform after page is completely loaded
-window.onload = function() {
-    
-   
-}
-
-
-
-/*  const greenBays = document.getElementById("greenBays");
-    const yellowBays = document.getElementById("yellowBays");
-    const redBays = document.getElementById("redBays");
-    const greenBaysOutput = document.getElementById("greenBaysOutput");
-    const yellowBaysOutput = document.getElementById("yellowBaysOutput");
-    const redBaysOutput = document.getElementById("redBaysOutput"); 
-    
-    if (greenBays) {
-        greenBays.addEventListener('input', e => {
-            greenBaysOutput.textContent = e.target.value;
-            console.log(greenBays.value);
-        });
-    }
-
-    if (yellowBays) {
-        yellowBays.addEventListener('input', e => {
-            yellowBaysOutput.textContent = e.target.value;
-        });
-    }
-
-    if (redBays) {
-        redBays.addEventListener('input', e => {
-            redBaysOutput.textContent = e.target.value;
-        });
-    }
-    
-    let currentPercentageOutput = document.getElementById("currentPercentage");
-    let currentPercentage = (greenBays.value + yellowBays.value) / (greenBays.value + yellowBays.value + redBays.value) * 100;
-    currentPercentageOutput.textContent = currentPercentage;
-    
-    */
-
-
-
+*/
